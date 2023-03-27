9a697a52676d682fd403bf7219f2e0d87532df61
Martin Dhollande <martindhollande@macbook-martin.home>
2023-03-27 22:39:25 +0200

    modifications légères

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -8,12 +8,12 @@ const token = { token: null, statusCode: 401 };
  * @returns Objet JSON contenant un token et un statut
  */
 
-const generateToken = async (user) => {
+const generateToken = async (user, duration) => {
   if (!user) {
     return token;
   }
   token.token = await jwt.sign(user, config.secret, {
-    expiresIn: "24h",
+    expiresIn: duration,
   });
   token.statusCode = 201;
   return token;
@@ -26,11 +26,18 @@ const generateToken = async (user) => {
  */
 
 // à finir
-const verifyToken = async (oldToken, user) => {
-  if (!token || !jwt.verify(oldToken, config.secret)) {
+const verifyRefreshToken = async (oldToken, user) => {
+  if (!oldToken || !jwt.verify(oldToken, config.secret)) {
     return token;
   }
-  return (token.token = generateToken(user));
+  return (token.token = generateToken(user, 24));
 };
 
-module.exports = { generateToken };
+const verifyAccessToken = async (accessToken, user) => {
+  if (!accessToken || !jwt.verify(accessToken, config.secret)) {
+    return token;
+  }
+  return true;
+};
+
+module.exports = { generateToken, verifyRefreshToken, verifyAccessToken };
